0a97873b0d6a6850c8393e573888ac10d8f2dc61
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-07 04:15:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your code adds up to #{total}"
 end
 
 def prompt_user
