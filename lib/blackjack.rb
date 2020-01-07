2f93c6bd30edc04a34e0cc577115d541b275f15a
Adam Xiao <adam.x.xiao@gmail.com>
2020-01-07 04:57:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,7 +55,10 @@ end
 
 def runner(card_total)
   welcome
-  initial_round
-  hit?(card_total)
-  display_card_total(card_total)
+  card_total = initial_round
+  if !(card_total > 21)
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
